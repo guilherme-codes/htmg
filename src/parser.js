4c06bb0b479eb3ba0a621c39e1d714ecb9aaf513
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-11-15 17:38:30 -0300

    feat: add docs

--- src/parser.js
+++ src/parser.js
@@ -1,3 +1,11 @@
+/**
+ * Parses the content of a file and replaces any includes with their corresponding partials.
+ * 
+ * @param {string} content - The content of the file.
+ * @param {Object} partials - An object containing the partials to be included.
+ * @param {Set} processedIncludes - A set of already processed includes to avoid circular dependencies.
+ * @returns {string} - The processed content with includes replaced by partials.
+ */
 export function parseFileContent(content, partials, processedIncludes = new Set()) {
   const includes = findIncludes(content)
 
@@ -9,7 +17,12 @@ export function parseFileContent(content, partials, processedIncludes = new Set(
     }, content)
 }
 
-
+/**
+ * Finds all include directives in the given content.
+ *
+ * @param {string} content - The content to search for include directives.
+ * @returns {Array} - An array of matches found.
+ */
 function findIncludes(content) {
   const includeRegex = /<!--\s*include:\s*(\w+)\s*-->/g
   const matches = []
@@ -22,6 +35,14 @@ function findIncludes(content) {
   return matches
 }
 
+/**
+ * Processes a partial by recursively parsing its content and its dependencies.
+ *
+ * @param {string} partialName - The name of the partial to process.
+ * @param {object} partials - An object containing all the partials.
+ * @param {Set} processedIncludes - A set containing the names of already processed partials.
+ * @returns {string} - The parsed content of the partial.
+ */
 function processPartial(partialName, partials, processedIncludes) {
   validatePartials(partials, partialName, processedIncludes)
   
@@ -36,6 +57,14 @@ function processPartial(partialName, partials, processedIncludes) {
 }
 
 
+/**
+ * Validates the existence of a partial and checks for circular includes.
+ *
+ * @param {Object} partials - The object containing all the partials.
+ * @param {string} partialName - The name of the partial to validate.
+ * @param {Set} processedIncludes - The set of processed includes.
+ * @throws {Error} If the partial is not found or a circular include is detected.
+ */
 function validatePartials(partials, partialName, processedIncludes) {
   if (!partials[partialName]) {
     console.warn(`Aviso: Partial "${partialName}" não encontrado`)
