5b49b6385295871734605862bdf7c1bd4c67ea55
Guilherme Guimarães Cazaroto <gui.cazaroto@gmail.com>
2024-12-08 22:44:56 -0300

    fix: test

--- tests/reader.test.js
+++ tests/reader.test.js
@@ -2,10 +2,13 @@ import assert from 'node:assert'
 import test from 'node:test'
 import { readLayouts } from '../core/reader.js'
 import { getExecBasePath } from '../utils/path.js'
-import { layoutsDir } from '../utils/contants.js'
+import env from '../utils/environment.js'
+import path from 'node:path'
 
 test('readLayouts returns the expected object', async () => {
-  const basePath = getExecBasePath(layoutsDir)
+  const basePath = getExecBasePath(
+    path.join('template', env.layoutsDir)
+  )
   const result = await readLayouts(basePath)
 
   assert.ok(result.default)
